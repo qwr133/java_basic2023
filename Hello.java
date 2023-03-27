452a4b645c5cf3f34d15dae647953701c7ba0d36
qwr133 <121946724+qwr133@users.noreply.github.com>
2023-03-27 10:38:27 +0900

    initial Commit

---
+++ Hello.java
@@ -0,0 +1,19 @@
+//alt +1
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요!"); //sout
+
+       // ctrl alt v
+        int a = 100;
+
+
+        Scanner sc = new Scanner();
+
+        //ctrl y = 줄삭제
+
+
+
+    }
+
+}
