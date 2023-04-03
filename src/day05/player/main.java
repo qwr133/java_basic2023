f97ef0628e3f85e3c53430096ad1349df9f94ef0
qwr133 <ksg165000@gmail.com>
2023-04-04 00:16:08 +0900

    home

--- src/day05/player/main.java
+++ src/day05/player/main.java
@@ -1,20 +1,17 @@
 package day05.player;
 
 public class main {
+
     public static void main(String[] args) {
 
-        player parking = new player(inputName : "주차왕파킹");
-        player gondue = new player(inputName : "딸긔겅듀");
+        player parking = new player("주차왕파킹");
+        player gondue = new player("딸긔겅듀");
+        System.out.println("===========================");
+        player noname = new player();
 
-        player noname= new player();
-
-        gondue.attack(parking);
-        System.out.println("parking의 주소"+ parking);
-        System.out.println("gondue 주소"+ this);
-
-
-
-
-//        System.out.println("주차왕 남은 체력: "+ parking.hp);
+        gondue.attack(gondue);
+        System.out.println("parking의 주소: " + parking);
+        System.out.println("gondue의 주소: " + gondue);
+//        System.out.println("주차왕 남은체력: " + parking.hp);
     }
 }
--- src/day05/player/player.java
+++ src/day05/player/player.java
@@ -1,25 +1,24 @@
 package day05.player;
 
 public class player {
-    //필드
-    String nickName;
-    int level;
-    int hp;
 
-    //생성자
-    public player(){
-        this(nickName : "이름 없음:"); //this(); - 나의 또 다른 생성자를 불러주세요
-        System.out.println("1번 생성자 호출!");
-        this.nickName="이름없음";
-        this.level=1;
-        this.hp=50;
+    // 필드
+    String nickName; // 캐릭터 닉네임
+    int level; // 레벨
+    int hp; // 체력
+
+    // 생성자
+    public player() {
+        this("이름없음");
+//        System.out.println("1번 생성자 호출!");
+//        this.nickName = "이름없음";
+//        this.level = 1;
+//        this.hp = 50;
     }
 
-    public player(String nickName){
-        this(nickName: "");
-        this(nickName, level:1, hp:50);
-        System.out.println("2번 생성자 호출!");
-
+    public player(String nickName) {
+        this(nickName, 1, 50);
+//        System.out.println("2번 생성자 호출!");
     }
 
     public player(String nickName, int level, int hp) {
@@ -28,33 +27,26 @@ public class player {
         this.hp = hp;
     }
 
-    player(String inputName){
-        nickName=inputName; //nickName.this (필드) // 지역번수에는 this 불가능
-        level=1;
-        hp=50;
+    // 기능 - 메서드
+    void levelUp(player this) {
+
     }
+    void attack(player target) {
 
-    //기능 - 메서드
-    void attack(player target){
+        if (this == target) return;
 
-        if(this==target) return;
+        System.out.println("target의 주소: " + target);
+        System.out.println("this의 주소: " + this);
 
-        System.out.println("target의 주소" + target);
-        System.out.println("this 주소" + this);
-        //맞은 녀석의 체력을 10~15 랜덤으로 감소시킴
-        int damage = (int) (Math.random()*6+10); //맞은 형식으로 변환 alt+enter
-
+        // 맞은녀석의 체력을 10~15 랜덤으로 감소시킬거임
+        int damage = (int) (Math.random() * 6 + 10);
         target.hp -= damage;
 
-
-
-        //전투로그를 출력할 예정
-        //"가해자가 피해자를 공격해서 xxx의 피해를 입힘"
-        System.out.printf("%s님이 %s님을 공격해서 %d의 피해를 입힘/n",
-                        this.nickName, target.nickName, damage );
-                    //this : 나 !!(myself)
-
-
+        // 전투로그를 출력할거임
+        // "가해자가 피해자를 공격해서 xxx의 피해를 입힘"
+        System.out.printf("%s님이 %s님을 공격해서 %d의 피해를 입힙!\n"
+                , this.nickName, target.nickName, damage);
     }
+
 
 }
